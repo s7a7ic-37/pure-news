021caf612c7e0a12e7384e94d1f128f650329dfc
Maks <mpasiecki@gmail.com>
2023-06-02 10:46:48 +0100

    revise voting logic, restyle UI

--- src/components/SingleArticleCard.jsx
+++ src/components/SingleArticleCard.jsx
@@ -15,23 +15,58 @@ const SingleArticleCard = ({ presentArticle }) => {
   const formattedDate = new Date(created_at).toLocaleString();
 
   const [newVotes, setNewVotes] = useState(votes);
-  const [hasVoted, setHasVoted] = useState(false);
+  const [voteChange, setVoteChange] = useState(0);
+  const [hasUpVoted, setHasUpVoted] = useState(false);
+  const [hasDownVoted, setHasDownVoted] = useState(false);
   const [isError, setIsError] = useState(false);
 
-  const changeVotes = (value) => {
-    if (!hasVoted) {
-      const updatedVotes = newVotes + value;
+  const upVote = () => {
+    setHasDownVoted(false);
+    if (voteChange === 0 || voteChange === -1) {
+      const updatedVotes = newVotes + 1;
       setNewVotes(updatedVotes);
       setIsError(false);
 
-      updateArticleVotes(article_id, { inc_votes: value })
-      .catch(() => {
-        setIsError(true)
+      updateArticleVotes(article_id, { inc_votes: 1 }).catch(() => {
+        setIsError(true);
       });
-      
-      setHasVoted(true);
+      if (voteChange === 0) {
+        setVoteChange(1);
+      } else {
+        setVoteChange(0);
+      }
+    } else {
+      setHasUpVoted(true);
     }
   };
+
+  const downVote = () => {
+    setHasUpVoted(false);
+    if (voteChange === 0 || voteChange === 1) {
+      setHasUpVoted(false);
+      const updatedVotes = newVotes - 1;
+      setNewVotes(updatedVotes);
+      setIsError(false);
+
+      updateArticleVotes(article_id, { inc_votes: -1 }).catch(() => {
+        setIsError(true);
+      });
+      if (voteChange === 0) {
+        setVoteChange(-1);
+      } else {
+        setVoteChange(0);
+      }
+    } else {
+      setHasDownVoted(true);
+    }
+  };
+
+  const upVoteButtonClassName = `vote-button ${
+    hasUpVoted ? "highlight-button" : ""
+  }`;
+  const downVoteButtonClassName = `vote-button ${
+    hasDownVoted ? "highlight-button" : ""
+  }`;
 
   return (
     <section id="article-card-info">
@@ -41,10 +76,20 @@ const SingleArticleCard = ({ presentArticle }) => {
       <p className="article-date">Posted at {formattedDate}</p>
       <article className="article-body">{body}</article>
       <img className="article-image" src={article_img_url} alt={title} />
-      <button className="vote-button" onClick={() => changeVotes(1)}>+</button>
+      <button
+        className={upVoteButtonClassName}
+        onClick={upVote}
+        disabled={hasUpVoted}
+      >+</button>
       <p className="article-votes">Votes: {newVotes}</p>
-      <button className="vote-button" onClick={() => changeVotes(-1)}>-</button>
-      {isError && <h3>Error has occurred, please try again later</h3>}  
+      <button
+        className={downVoteButtonClassName}
+        onClick={downVote}
+        disabled={hasDownVoted}
+      >-</button>
+      {hasUpVoted && (<h3 className="error-message">You have already up-voted</h3>)}
+      {hasDownVoted && (<h3 className="error-message">You have already down-voted</h3>)}
+      {isError && <h3>Error has occurred, please try again later</h3>}
     </section>
   );
 };
